eb32cb5b0a2120dfec99b449229d46dd4b073720
moritz <sergeial1972@gmail.com>
2025-12-23 20:07:50 +0300

    1/4 finished

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ import logging
 from aiogram import types, Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message, message
-from bot_ext.handlers import router
+from bot_ext.handlers import router as handlers_router
+from bot_ext.host_managment import router as host_managment_router
 
 #dotenv
 load_dotenv()
@@ -22,7 +23,9 @@ bot = Bot(token=bot_token)
 dp = Dispatcher()
 
 async def main():
-    dp.include_router(router)
+    dp.include_router(handlers_router)
+    dp.include_router(host_managment_router)
+
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
